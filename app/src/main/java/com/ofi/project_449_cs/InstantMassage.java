0453f9cbc9626bce90744e3e4226b22ab859a430
Ofi19 <ms-2016-a@hotmail.com>
2018-03-18 15:47:04 -0500

    Iteration#3

---
+++ app/src/main/java/com/ofi/project_449_cs/InstantMassage.java
@@ -0,0 +1,28 @@
+package com.ofi.project_449_cs;
+
+
+
+ class InstantMassage {
+     private String message;
+     private String author;
+
+     InstantMassage(String message, String author) {
+         this.message = message;
+         this.author = author;
+     }
+
+     public InstantMassage() {
+
+
+
+     }
+
+     String getMessage() {
+         return message;
+     }
+
+     String getAuthor() {
+         return author;
+     }
+ }
+
